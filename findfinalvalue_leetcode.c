d631a03d39437148dd7c77f8fc49490618dca34c
Nicolas Mattozo <nicolasmattozo05@hotmail.com>
2024-11-30 23:17:17 -0300

    findfinalvalue_leetcode

---
+++ findfinalvalue_leetcode.c
@@ -0,0 +1,11 @@
+int findFinalValue(int* nums, int numsSize, int original) {
+    
+    for(int i = 0; i < numsSize; i++)
+    {
+        if(nums[i] == original){
+                original = original * 2;
+                i = -1;
+             }
+    }
+    return original;
+}
