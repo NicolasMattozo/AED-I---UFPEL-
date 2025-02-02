cf3db6c6ccbc1a26c19edd737065e2f05928294d
Nicolas Mattozo <nicolasmattozo05@hotmail.com>
2025-02-02 19:57:03 -0300

    Agenda_pBuffer Final

--- Agenda-pBuffer/src/Agenda_pBuffer.c
+++ Agenda-pBuffer/src/Agenda_pBuffer.c
@@ -2,102 +2,100 @@
 #include <string.h>
 #include <stdlib.h>
 
-// pBuffer = ((op) + (flag) + (tempNome) + (tempIdade) + (tempEmail) + (busca) + (dados))
+/*
+====================
+AGENDA PBUFFER
 
+Nome: Nicolas Mattozo
+Curso: Ciência da Computação - UFPEL
+
+ORGANIZAÇÃO DO BUFFER = (op) + (flag) + (tempNome) + (tempIdade) + (tempEmail) + (busca) + (dados)
+====================
+*/
 int main( void ) {
-
-	void *pBuffer = malloc(
-		sizeof( int ) +	// op
-		sizeof( int ) +	// flag
-		( 50 * sizeof( char ) ) + // tempNome
-		sizeof( int ) + // tempIdade
-		( 50 * sizeof( char ) ) + // tempEmail
-		( 100 * sizeof( char ) ) + // busca
-		( 100 * sizeof( char ) ) // dados
-	);
+	void *pBuffer = malloc( 312 );
 
 	if ( pBuffer == NULL ) {
 		printf( "erro de alocacao / memoria insuficiente" );
+		exit( 1 );
 	}
 
-	// VOU CRIAR PONTEIROS PARA APONTAR PARA O INICIO DE CADA DADO DENTRO DO pBuffer
-	void *op        = pBuffer; // aponta p inicio do pBuffer
-	void *flag      = ( char * ) op + sizeof( int ); // aponta pro inicio de flag
-	void *tempNome  = ( char * ) flag + sizeof( int ); // aponta pro inicio de tempNome
-	void *tempIdade = ( char * ) tempNome + ( 50 * sizeof( char ) ); // aponta pro inicio de tempIdade
-	void *tempEmail = ( char * ) tempIdade + sizeof( int ); // aponta pro inicio de tempEmail
-	void *busca     = ( char * ) tempEmail + ( 50 * sizeof( char ) ); // aponta pro inicio de busca
-	void *dados     = ( char * ) busca + ( 100 * sizeof( char ) ); // aponta pro inicio de dados
-	void *contador  = dados; // ponteiro pro inicio de dados que vai atualizando sempre pro ultimo caracter
+	// Ponteiros para cada dado dentro do pBuffer
+	int *op = ( int * )pBuffer;
+	int *flag = ( int * )( ( char * )pBuffer + sizeof( int ) );
+	char *tempNome = ( char * )flag + sizeof( int );
+	int *tempIdade = ( int * )( tempNome + 50 );
+	char *tempEmail = ( char * )tempIdade + sizeof( int );
+	char *busca = tempEmail + 50;
+	char *dados = busca + 100;
+	char *contador = dados; // Atualiza conforme necessário
 
 	// Inicializa as variáveis
-	*( int * ) op = 0;    // Inicializa op
-    *( int * ) flag = 0;  // Inicializa flag
-    memset( tempNome, 0, 50 );  // Limpa tempNome
-    *( int * )tempIdade = 0;   // Inicializa tempIdade
-    memset( tempEmail, 0, 50 ); // Limpa tempEmail
-    memset( busca, 0, 100 );    // Limpa busca
-    memset( dados, 0, 100 );    // Limpa dados
+	*op = 0;          // Inicializa op
+	*flag = 0;        // Inicializa flag
+	memset( tempNome, 0, 50 );  // Limpa tempNome
+	*tempIdade = 0;   // Inicializa tempIdade
+	memset( tempEmail, 0, 50 ); // Limpa tempEmail
+	memset( busca, 0, 100 );    // Limpa busca
+	memset( dados, 0, 100 );    // Limpa dados
 
 	for ( ;; ) {
-        printf( "1- Adicionar Pessoa (Nome, Idade, Email)\n" );
-        printf( "2- Remover Pessoa\n" );
-        printf( "3- Buscar Pessoa\n" );
-        printf( "4- Listar todos\n" );
-        printf( "5- Sair\n" );
+		printf( "1- Adicionar Pessoa (Nome, Idade, Email)\n" );
+		printf( "2- Remover Pessoa\n" );
+		printf( "3- Buscar Pessoa\n" );
+		printf( "4- Listar todos\n" );
+		printf( "5- Sair\n" );
 
-		*( int * ) flag = 0;  // Inicializa flag
+		*flag = 0;  // Inicializa flag
 
-        scanf( "%3d", ( int * ) op );
+		scanf( "%3d", op );
 
-		switch ( *(int * ) op ) {
-
-			case 1: {
-
+		switch ( *op ) {
+			case 1: {    //Adicionar pessoa
 				printf( "Digite o Nome: " );
-				scanf( "%49s", ( char * ) tempNome );
+				scanf( "%49s", tempNome );
 
 				printf( "Digite a Idade: " );
-				scanf( "%d", ( int * ) tempIdade );
+				scanf( "%d", tempIdade );
+				getchar();
 
-				printf("Digite seu Email: ");
-				scanf( "%49s", ( char * ) tempEmail );
+				printf( "Digite seu Email: " );
+				scanf( "%49s", tempEmail );
 
-				// Calcula o novo tamanho necessário do buffer
-				void *novo_fim = ( char * ) contador 
-				+ strlen( ( char * ) tempNome ) + 1  // Nome
-		        + sizeof( int )                 // Idade
-		        + strlen( ( char * ) tempEmail ) + 1; // Email
+				void *novoFim = ( char * )contador //Calcula o novo fim para realocar memória
+					+ strlen( tempNome ) + 1  // Nome
+					+ sizeof( int )           // Idade
+					+ strlen( tempEmail ) + 1; // Email
 
 				// Realoca pBuffer
-				pBuffer = realloc( pBuffer, ( char * ) novo_fim - ( char * ) pBuffer );
-    
-				if ( pBuffer == NULL ) {
-			        printf( "Erro de alocacao / memoria insuficiente\n" );
-			        exit( 1 );
-		    	}
-    
-				// Atualiza pBuffer e ajusta os ponteiros
-				op        = pBuffer;
-				flag      = ( char * ) op + sizeof( int );
-				tempNome  = ( char * ) flag + sizeof( int );
-				tempIdade = ( char * ) tempNome + ( 50 * sizeof( char ) );
-				tempEmail = ( char * ) tempIdade + sizeof( int );
-				busca     = ( char * ) tempEmail + ( 50 * sizeof( char ) );
-				dados     = ( char * ) busca + ( 100 * sizeof( char ) );
+				void *novoPBuffer = realloc( pBuffer, ( char * )novoFim - ( char * )pBuffer );
 
-				// Atualiza contador para apontar para o fim correto
-				*( char * ) contador = ( char * ) novo_fim - ( char * ) pBuffer + ( ( char * ) contador - ( char * ) dados ) + 1;
+				if ( novoPBuffer == NULL ) {
+					printf( "Erro de alocacao / memoria insuficiente\n" );
+					exit( 1 );
+				} else {
+					contador = ( char * )novoPBuffer + ( ( char * )contador - ( char * )pBuffer ); //Atualiza Contador
+					pBuffer = novoPBuffer;
+				}
+
+				// Atualiza pBuffer e ajusta os ponteiros
+				op = ( int * )pBuffer;
+				flag = ( int * )( ( char * )pBuffer + sizeof( int ) );
+				tempNome = ( char * )flag + sizeof( int );
+				tempIdade = ( int * )( tempNome + 50 );
+				tempEmail = ( char * )tempIdade + sizeof( int );
+				busca = tempEmail + 50;
+				dados = busca + 100;
 
 				// Copia os novos dados para a área realocada
-				memcpy( contador, tempNome, strlen( ( char *) tempNome ) + 1 );
-				contador = ( char * ) contador + strlen( ( char * ) tempNome ) + 1;
+				memcpy( contador, tempNome, strlen( tempNome ) + 1 ); // Copia o nome
+				contador += strlen( tempNome ) + 1;
 
-				memcpy( contador, tempIdade, sizeof( int ) );
-				contador = ( char * ) contador + sizeof( int );
+				memcpy( contador, tempIdade, sizeof( int ) ); // Copia a idade
+				contador += sizeof( int );
 
-				memcpy( contador, tempEmail, strlen( ( char * ) tempEmail ) +  1);
-				contador = ( char * ) contador + strlen( ( char * ) tempEmail ) + 1;
+				memcpy( contador, tempEmail, strlen( tempEmail ) + 1 ); // Copia o email
+				contador += strlen( tempEmail ) + 1;
 
 				printf( "Cadastro adicionado com sucesso\n" );
 
@@ -106,57 +104,61 @@ int main( void ) {
 
 			case 2: {
 				printf( "Digite o nome que deseja excluir: " );
-				scanf( "%99s", ( char * ) busca );
+				scanf( "%99s", busca );
 
-				void *imprime = dados; //Imprime aponta para o inicio de dados
+				char *imprime = dados; // Imprime aponta para o inicio de dados
 				void *fim = NULL;
 
-				while ( imprime < contador ) {
+				while ( ( char * )imprime < ( char * )contador ) {
 					if ( strcmp( imprime, busca ) == 0 ) {
-
 						printf( "NOME ENCONTRADO \n" );
-						
-						*( int * ) flag = 1;
+
+						*flag = 1;
 
 						void *auxiliar = NULL;
-						auxiliar = imprime; //auxiliar recebe o endereço do primeiro caracter do nome buscado
-						auxiliar += strlen( ( char * ) auxiliar ) + 1;
-						auxiliar += sizeof( char ) * sizeof( int );
-						auxiliar += strlen( ( char * ) auxiliar ) + 1;
+						auxiliar = imprime; // Auxiliar recebe o endereço do primeiro caracter do nome buscado
+						auxiliar += strlen( ( char * )auxiliar ) + 1;
+						auxiliar += 4;
+						auxiliar += strlen( ( char * )auxiliar ) + 1;
 
-						fim = auxiliar;  //fim aponta para o endereço do ultimo caracter do nome buscado
+						fim = auxiliar;  // Fim aponta para o endereço do ultimo caracter do nome buscado
 
-						memmove( imprime, fim, ( char * ) contador - ( char * ) fim );
+						memmove( imprime, fim, ( char * )contador - ( char * )fim );
 
-						contador = ( char * )contador - ( ( char * ) fim - ( char * ) imprime );
+						contador = ( char * )contador - ( ( char * )fim - ( char * )imprime ); //Atualiza contador
 
-						pBuffer = realloc( pBuffer, ( char * ) contador - ( char * ) pBuffer );
+						void *novoPBuffer = realloc( pBuffer, ( char * )contador - ( char * )pBuffer );
 
-						if ( pBuffer == NULL ) {
+						if ( novoPBuffer == NULL ) {
 							printf( "falha ao alocar memoria\n" );
 							exit( 1 );
 						}
 
-						op        = pBuffer;
-						flag      = ( char * ) op + sizeof( int );
-						tempNome  = ( char * ) flag + sizeof( int );
-						tempIdade = ( char * ) tempNome + ( 50 * sizeof( char ) );
-						tempEmail = ( char * ) tempIdade + sizeof( int );
-						busca     = ( char * ) tempEmail + ( 50 * sizeof( char ) );
+						pBuffer = novoPBuffer;
+
+						op = ( int * )pBuffer;
+						flag = ( int * )( ( char * )pBuffer + sizeof( int ) );
+						tempNome = ( char * )flag + sizeof( int );
+						tempIdade = ( int * )( tempNome + 50 );
+						tempEmail = ( char * )tempIdade + sizeof( int );
+						busca = tempEmail + 50;
+						dados = busca + 100;
+						if ( contador == dados ) {
+							contador = dados;
+						}
 
 						printf( "Registro excluido com sucesso\n" );
 
 						break;
-
 					} else {
 						// Caso o nome não seja encontrado, continua verificando
-                        imprime += strlen( ( char * ) imprime ) + 1;  // Avança para o próximo campo (nome)
-                        imprime += sizeof( int );                   // Avança para o próximo campo (idade)
-                        imprime += strlen( ( char * ) imprime ) + 1;  // Avança para o próximo campo (email)
+						imprime += strlen( ( char * )imprime ) + 1;  // Avança para o próximo campo (nome)
+						imprime += sizeof( int );                   // Avança para o próximo campo (idade)
+						imprime += strlen( ( char * )imprime ) + 1;  // Avança para o próximo campo (email)
 					}
 				}
 
-				if ( *( int * ) flag == 1 ) {
+				if ( *flag == 1 ) {
 					printf( "--------------------------\n" );
 				} else {
 					printf( "--------------------------\n" );
@@ -169,34 +171,32 @@ int main( void ) {
 
 			case 3: {
 				printf( "Digite o nome que deseja buscar: \n" );
-				scanf( "%99s", ( char * ) busca );
+				scanf( "%99s", busca );
 
-				void *imprime = dados;
-				while ( imprime < contador ) {
-					if ( strcmp( imprime, busca ) == 0 ) {
-
+				char *imprime = dados;
+				while ( ( char * )imprime < ( char * )contador ) {
+					if ( strcmp( imprime, busca ) == 0 ) { //Compara os nomes
 						printf( "NOME ENCONTRADO \n \n" );
 
-						printf( "Nome: %s\n", ( char * ) imprime );
-						imprime += strlen( ( char * ) imprime ) + 1;
+						printf( "Nome: %s\n", ( char * )imprime );
+						imprime += strlen( ( char * )imprime ) + 1;
 
-						printf( "Idade: %d\n", *( int * )( char * ) imprime );
-						imprime += sizeof( int );
+						printf( "Idade: %d\n", *( int * )( char * )imprime );
+						imprime += 4;
 
-						printf( "Email: %s\n", ( char * ) imprime );
-						imprime += strlen( ( char * ) imprime ) + 1;
+						printf( "Email: %s\n", ( char * )imprime );
+						imprime += strlen( ( char * )imprime ) + 1;
 
-						*( int * ) flag = 1;
-
+						*flag = 1;
 					} else {
 						// Caso o nome não seja encontrado, continua verificando
-                        imprime += strlen( ( char * ) imprime ) + 1;  // Avança para o próximo campo (nome)
-                        imprime += sizeof( int );                   // Avança para o próximo campo (idade)
-                        imprime += strlen( ( char * ) imprime ) + 1;  // Avança para o próximo campo (email)
+						imprime += strlen( ( char * )imprime ) + 1;  // Avança para o próximo campo (nome)
+						imprime += sizeof( int );                   // Avança para o próximo campo (idade)
+						imprime += strlen( ( char * )imprime ) + 1;  // Avança para o próximo campo (email)
 					}
 				}
 
-				if ( *( int * ) flag == 1 ) {
+				if ( *flag == 1 ) {
 					printf( "--------------------------\n" );
 				} else {
 					printf( "--------------------------\n" );
@@ -208,9 +208,9 @@ int main( void ) {
 			}
 
 			case 4: {
-				void *imprime = dados;
+				char *imprime = dados;
 
-				if ( imprime == contador ) {
+				if ( imprime == contador || *imprime == '\0' ) {
 					printf( "--------------------------\n" );
 					printf( "Nao existem pessoas cadastradas\n" );
 					printf( "--------------------------\n" );
@@ -218,15 +218,14 @@ int main( void ) {
 				}
 
 				while ( imprime < contador ) {
+					printf( "Nome: %s\n", ( char * )imprime );
+					imprime += strlen( ( char * )imprime ) + 1;
 
-					printf( "Nome: %s\n", ( char * ) imprime );
-					imprime += strlen( ( char * ) imprime ) + 1;
-
-					printf( "Idade: %d\n", *( int * )( char * ) imprime );
+					printf( "Idade: %d\n", *( int * )imprime );
 					imprime += sizeof( int );
 
-					printf( "Email: %s\n", ( char * ) imprime );
-					imprime += strlen( ( char * ) imprime ) + 1;
+					printf( "Email: %s\n", ( char * )imprime );
+					imprime += strlen( ( char * )imprime ) + 1;
 
 					printf( "--------------------------\n" );
 				}
